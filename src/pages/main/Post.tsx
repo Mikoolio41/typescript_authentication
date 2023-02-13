5e4d3268698359a1b924337dffd908ef1876c682
Mika Bibas <mikabibas@gmail.com>
2023-02-13 20:32:27 +0200

    like button

--- src/pages/main/Post.tsx
+++ src/pages/main/Post.tsx
@@ -10,7 +10,10 @@ export const Post = (props: Props) => {
     <div className="postContainer">
       <h3 className="postTitle">{post.title}</h3>
       <p className="postDescription">{post.description}</p>
-      <p className="postUsername">by: {post.username}</p>
+      <div className="userLikeSection">
+        <p className="postUsername">by: {post.username}</p>
+        <button className="likeBtn">&#128077;</button>
+      </div>
     </div>
   );
 };
